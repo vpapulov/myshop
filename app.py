c9a677ebc3cc487ffd776fc6defdd54b593662c6
Vladlen Papulov <vladlen.papulov@gmail.com>
2020-11-21 14:27:36 +0300

    login page

--- app.py
+++ app.py
@@ -1,30 +1,17 @@
-from flask import Flask, render_template, flash, redirect, url_for
-from config import Config
-from forms import LoginForm
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
+from config import Config
+import routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-from models import User
-
-@app.route('/')
-def index():
-    return render_template('index.html', title='Магазин')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Вход', form=form)
-
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,8 +1,11 @@
-from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db, login_manager
 from datetime import datetime
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
@@ -11,6 +14,17 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class Order(db.Model):
