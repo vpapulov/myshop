ae078c5126029f36a9b5a02e13db5bc7345bf655
Vladlen Papulov <vladlen.papulov@gmail.com>
2021-01-02 12:23:06 +0500

    отказался от поля SelectField в пользу QuerySelectField, теперь form.populate_obj(order) корретно отрабатывает, плюс большая гибкость поля

--- project/forms/order.py
+++ project/forms/order.py
@@ -1,17 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateTimeField, SelectField
+from wtforms import StringField, SubmitField, DateTimeField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired
 
+from project.models.order import Customer
 
-class NewOrderForm(FlaskForm):
-    date_order_placed = DateTimeField('Дата', validators=[DataRequired()], format='%d.%m.%Y %H:%M:%S')
-    customer = SelectField('Покупатель', validators=[DataRequired()], coerce=int)
-    comment = StringField('Комментарий', validators=[DataRequired()])
-    submit = SubmitField('Создать')
+
+def customer_query():
+    return Customer.query
 
 
 class OrderForm(FlaskForm):
     date_order_placed = DateTimeField('Дата', validators=[DataRequired()], format='%d.%m.%Y %H:%M:%S')
-    customer = SelectField('Покупатель', validators=[DataRequired()], coerce=int)
+    customer = QuerySelectField('Покупатель', validators=[DataRequired()],
+                                query_factory=customer_query,
+                                get_label='name')
     comment = StringField('Комментарий', validators=[DataRequired()])
     submit = SubmitField('Сохранить')
--- project/routes/order.py
+++ project/routes/order.py
@@ -1,9 +1,11 @@
+from datetime import datetime
+
 from flask_login import login_required, current_user
-from flask import render_template, Blueprint, request, flash, redirect, url_for
+from flask import render_template, Blueprint, flash, redirect, url_for
 
 from project import db
-from project.forms.order import NewOrderForm, OrderForm
-from project.models.order import Order, Customer
+from project.forms.order import OrderForm
+from project.models.order import Order
 
 orders_blueprint = Blueprint('orders', __name__, url_prefix='/orders', template_folder='templates')
 
@@ -19,8 +21,7 @@ def order_list():
 @login_required
 def order_edit(order_id):
     order = Order.query.filter_by(id=order_id).first_or_404()
-    form = OrderForm(request.form, obj=order)
-    form.customer.choices = [(c.id, c.name) for c in Customer.query.all()]
+    form = OrderForm(obj=order)
     if form.validate_on_submit():
         form.populate_obj(order)
         db.session.add(order)
@@ -32,17 +33,12 @@ def order_edit(order_id):
 
 @orders_blueprint.route('/new', methods=['GET', 'POST'])
 @login_required
-def new_order():
-    form = NewOrderForm()
-    form.customer.choices = [(c.id, c.name) for c in Customer.query.all()]
+def order_new():
+    order = Order(date_order_placed=datetime.now())
+    form = OrderForm(obj=order)
     if form.validate_on_submit():
-        order = Order(
-            date_order_placed=form.date_order_placed.data,
-            customer_id=form.customer.data,
-            comment=form.comment.data,
-            user_id=current_user.id
-        )
-        # form.populate_obj(order)
+        form.populate_obj(order)
+        order.user_id = current_user.id
         db.session.add(order)
         db.session.commit()
         flash('Заказ успешно создан.')
