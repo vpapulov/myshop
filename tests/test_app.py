6291b84db9c2e4247836cf3e065ab6a843a64c3f
Vladlen Papulov <vladlen.papulov@gmail.com>
2021-01-17 16:49:50 +0500

    для QuerySelectField перешал на wtforms_sqlalchemy запустил тесты (пока пустые)

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,52 @@
 import os
+import sys
 import tempfile
-
 import pytest
 
-from context import app
+sys.path.insert(1,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import create_app
+from project.models.user import User
+from project import db
 
 
 @pytest.fixture
-def client():
-    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+def test_client():
+    db_fd, db_fn = tempfile.mkstemp()
+    app = create_app()
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_fn \
+                                            + '?check_same_thread=False'
     app.config['TESTING'] = True
+    app.config['BCRYPT_LOG_ROUNDS'] = 4  # Bcrypt algorithm hashing rounds
+    app.config['WTF_CSRF_ENABLED'] = False  # Disable CSRF tokens in the Forms
 
     with app.test_client() as client:
         with app.app_context():
-            app.init_db()
+            db.init_app(app)
         yield client
 
     os.close(db_fd)
-    os.unlink(app.config['DATABASE'])
+    os.unlink(app.config['SQLALCHEMY_DATABASE_URI'])
+
+
+@pytest.fixture(scope='module')
+def init_database():
+
+    db.create_all()
+
+    user1 = User(username='test1', email='test1@gmail.com')
+    db.session.add(user1)
+
+    db.session.commit()
+
+    yield db
+
+    db.drop_all()
+
+
+def test_home(test_client, init_database):
+    r = test_client.get('/')
+    assert r.status_code == 302
+    user1 = User.query.first()
+    assert user1.username == 'test`'
